2cdb3fe37786622129f84d9d238ca13d6a25f18f
Erik Sundius <43874589+esundius@users.noreply.github.com>
2025-08-03 19:04:55 +0300

    Fixed table in database schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    username TEXT CHECK (username != '') NOT NULL UNIQUE,
+    id            INTEGER PRIMARY KEY,
+    username      TEXT CHECK (username != '') NOT NULL UNIQUE,
     password_hash TEXT CHECK (password_hash != '') NOT NULL,
-    created TEXT DEFAULT CURRENT_TIMESTAMP
+    created       TEXT DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE recipes {
-    id INTEGER PRIMARY KEY,
-    content TEXT,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    created TEXT DEFAULT CURRENT_TIMESTAMP
-}
+CREATE TABLE recipes (
+    id            INTEGER PRIMARY KEY,
+    name          TEXT,
+    content       TEXT,
+    user_id       INTEGER,
+    created       TEXT DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
