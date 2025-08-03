6935a704038a53182d789a1455bff9e8029402d4
Erik Sundius <43874589+esundius@users.noreply.github.com>
2025-08-03 18:59:00 +0300

    Add table to database schema

--- schema.sql
+++ schema.sql
@@ -3,4 +3,11 @@ CREATE TABLE users (
     username TEXT CHECK (username != '') NOT NULL UNIQUE,
     password_hash TEXT CHECK (password_hash != '') NOT NULL,
     created TEXT DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE TABLE recipes {
+    id INTEGER PRIMARY KEY,
+    content TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    created TEXT DEFAULT CURRENT_TIMESTAMP
+}
