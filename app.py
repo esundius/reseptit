f215f8af096b93096a5b7adc8bf0cf6ca7352c8e
Erik Sundius <43874589+esundius@users.noreply.github.com>
2025-08-03 14:27:36 +0300

    Add routes for registration pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/registration')
+def registration():
+    return render_template('user_reg_form.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    username = request.form['username']
+    return render_template('user_reg_result.html', username=username)
