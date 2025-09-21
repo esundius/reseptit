d2f23034bf6856f022d74de4451ca872f4773634
Erik Sundius <43874589+esundius@users.noreply.github.com>
2025-09-21 17:20:10 +0300

    Refactor HTML templates and some methods some more

--- app.py
+++ app.py
@@ -14,50 +14,56 @@ def index():
     recipes = db.query(sql)
     return render_template('index.html.j2', recipes=recipes)
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'GET':
+        return render_template('login.html.j2')
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    sql = 'SELECT id, password_hash FROM users WHERE username = ?'
-    result = db.query(sql, (username,))
-    if not result:
-        return 'VIRHE: väärä tunnus tai salasana'
-    user_id = result[0]['id']
-    password_hash = result[0]['password_hash']
+        sql = 'SELECT id, password_hash FROM users WHERE username = ?'
+        result = db.query(sql, (username,))
+        if not result:
+            return 'ERROR: User not found or wrong password'
+        user_id = result[0]['id']
+        password_hash = result[0]['password_hash']
 
-    if check_password_hash(password_hash, password):
-        session['user_id'] = user_id
-        session['username'] = username
-        return redirect('/')
-    else:
-        return 'VIRHE: väärä tunnus tai salasana'
+        if check_password_hash(password_hash, password):
+            session['user_id'] = user_id
+            session['username'] = username
+            return redirect('/')
+        else:
+            return 'ERROR: User not found or wrong password'
 
 @app.route('/logout')
 def logout():
     del session['username']
+    del session['user_id']
     return redirect('/')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html.j2')
-
-@app.route('/create', methods=['POST'])
-def create():
-    username = request.form['username']
-    password1 = request.form['password1']
-    password2 = request.form['password2']
-    if password1 != password2:
-        return 'VIRHE: salasanat eivät ole samat'
-    password_hash = generate_password_hash(password1)
-
-    try:
-        sql = 'INSERT INTO users (username, password_hash) VALUES (?, ?)'
-        db.execute(sql, (username, password_hash))
-    except sqlite3.IntegrityError:
-        return 'VIRHE: tunnus on jo varattu'
+    if request.method == 'GET':
+        return render_template('register.html.j2')
     
-    return render_template('create.html.j2')
+    if request.method == 'POST':
+        username = request.form['username']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if password1 != password2:
+            return 'ERROR: The passwords do not match!'
+        password_hash = generate_password_hash(password1)
+
+        try:
+            sql = 'INSERT INTO users (username, password_hash) VALUES (?, ?)'
+            db.execute(sql, (username, password_hash))
+        except sqlite3.IntegrityError:
+            return 'ERROR: User name already taken'
+
+        return render_template('create.html.j2')
 
 @app.route('/add_recipe', methods=['POST'])
 def add_recipe():
