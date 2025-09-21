2fc285aa0300e1fdc4c0d80ff2d3a370bafc270e
Erik Sundius <43874589+esundius@users.noreply.github.com>
2025-09-21 18:35:20 +0300

    Add search, edit and remove functionality

--- app.py
+++ app.py
@@ -65,19 +65,93 @@ def register():
 
         return render_template('create.html.j2')
 
-@app.route('/add_recipe', methods=['POST'])
+@app.route('/add_recipe', methods=['GET', 'POST'])
 def add_recipe():
-    name = request.form['name']
-    content = request.form['content']
-    user_id = session['user_id']
-    sql = 'INSERT INTO recipes (name, content, user_id) VALUES (?, ?, ?)'
-    db.execute(sql, (name, content, user_id))
-    return redirect('/')
+    if request.method == 'GET':
+        return render_template('add_recipe.html.j2')
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        content = request.form['content']
+        user_id = session['user_id']
+        sql = 'INSERT INTO recipes (name, content, user_id) VALUES (?, ?, ?)'
+        db.execute(sql, (name, content, user_id))
+        return redirect('/')
 
 @app.route('/recipe/<int:recipe_id>')
 def show_recipe(recipe_id):
-    sql = '''SELECT r.id, r.name, r.content, r.created, r.user_id, u.username
+    sql = '''SELECT r.id,
+                    r.name,
+                    r.content,
+                    r.created,
+                    r.user_id,
+                    u.username
              FROM recipes r, users u
-             WHERE r.user_id = u.id AND r.id = ?'''
+             WHERE r.user_id = u.id AND
+                   r.id = ?'''
     result = db.query(sql, (recipe_id,))[0]
     return render_template('recipe.html.j2', recipe=result)
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    results = []
+    if query:
+        sql = '''SELECT r.id,
+                        r.name,
+                        r.created,
+                        r.user_id,
+                        u.username
+                 FROM recipes r, users u
+                 WHERE r.user_id = u.id AND
+                       r.name LIKE ?
+                 ORDER BY r.name ASC'''
+        results = db.query(sql, ['%' + query + '%'])
+    return render_template('search.html.j2', query=query, results=results)
+
+@app.route('/edit/<int:recipe_id>', methods=['GET', 'POST'])
+def edit_recipe(recipe_id):
+    sql = '''SELECT r.id,
+                    r.name,
+                    r.content,
+                    r.created,
+                    r.user_id,
+                    u.username
+             FROM recipes r, users u
+             WHERE r.user_id = u.id AND
+                   r.id = ?'''
+    recipe = db.query(sql, (recipe_id,))[0]
+
+    if request.method == 'GET':
+        return render_template('edit_recipe.html.j2', recipe=recipe)
+    
+    if request.method == 'POST':
+        if 'save' in request.form:
+            name = request.form['name']
+            content = request.form['content']
+            sql = '''UPDATE recipes SET name = ?, content = ? WHERE id = ?'''
+            db.execute(sql, [name, content, recipe_id])
+        return redirect('/recipe/' + str(recipe_id))
+
+@app.route('/remove/<int:recipe_id>', methods=['GET', 'POST'])
+def remove_recipe(recipe_id):
+    sql = '''SELECT r.id,
+                    r.name,
+                    r.content,
+                    r.created,
+                    r.user_id,
+                    u.username
+             FROM recipes r, users u
+             WHERE r.user_id = u.id AND
+                   r.id = ?'''
+    recipe = db.query(sql, (recipe_id,))[0]
+
+    if request.method == 'GET':
+        return render_template('remove_recipe.html.j2', recipe=recipe)
+    
+    if request.method == 'POST':
+        if 'continue' in request.form:
+            sql = '''DELETE FROM recipes WHERE id = ?'''
+            db.execute(sql, [recipe_id])
+        return redirect('/')
+
